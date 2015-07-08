6da326b743fe945aeceb095b835c923592e25134
Julian Poyourow <julianpoyo@gmail.com>
2015-07-08 13:49:04 -0700

    Added basic resources

--- public/modules/giftcards/services/giftcards.client.service.js
+++ public/modules/giftcards/services/giftcards.client.service.js
@@ -3,9 +3,28 @@
 //giftcards service used to communicate Giftcards REST endpoints
 angular.module('giftcards').factory('Giftcards', ['$resource',
 	function($resource) {
-		return $resource('giftcards/:giftcardId', { giftcardId: '@_id'
-		}, {
-			
+		return $resource('/giftcards/:giftcardId',
+		{ giftcardId: '@giftcardId' }, {
+			getGiftcards: {
+				method: 'GET',
+				params: { giftcardId: "" }
+			},
+			getGiftcard: {
+				method: 'GET',
+				params: { giftcardId: "" }
+			},
+			createGiftcard: {
+				method: 'POST',
+				params: { giftcardId: "@giftcardId" }
+			},
+			spendGiftcard: {
+				method: 'PUT',
+				params: { giftcardId: "@giftcardId" }
+			},
+			deleteGiftcard: {
+				method: 'DELETE',
+				params: { giftcardId: "@giftcardId" }
+			}
 		});
 	}
 ]);
