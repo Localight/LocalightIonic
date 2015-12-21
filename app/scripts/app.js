b53a9224424a48890e3cfd8949a4dcc047c08871
Aaron <torch2424@hotmail.com>
2015-12-20 17:11:13 -0800

    Ionic JS Scrolling
    Disabling IOS scrolling made things worse, so put back to normal.
    Also finishing up for the day

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -222,10 +222,12 @@ angular
       // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
+
       // Don't remove this line unless you know what you are doing. It stops the viewport
       // from snapping when text inputs are focused. Ionic handles this internally for
       // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
+      //Made keyboard worse
+      //cordova.plugins.Keyboard.disableScroll(true);
     }
     if(window.StatusBar) {
     // Set the statusbar to use the default style, tweak this to
